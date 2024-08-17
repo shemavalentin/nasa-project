d7464d05ebfa937bee13403b131afe5c6c1607e5
Shema N. Valentin <shemavalentin@gmail.com>
2024-08-17 18:05:50 +0200

    Solved CORS bugs

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const planetRouter = require("./routes/planets/planet.router");
 
 const app = express();
+
+// set CORS to whitelist request from different origins
+// But we need to not allow everything enter our API, we must add set of security.
+app.use(
+  cors({
+    // Allowing request from specific origin
+    origin: "http://localhost:3000",
+  })
+);
+
 // Here I will get the flexibility to pass express as a middleware when receiving the
 // JSON requests.
 app.use(express.json());
