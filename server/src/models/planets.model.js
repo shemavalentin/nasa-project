5fdb55a6957f7573f65510024edc838c3e118a10
Shema N. Valentin <shemavalentin@gmail.com>
2024-09-03 09:53:24 +0200

    Inserting and Updating Documents in Mongo

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,6 +2,12 @@ const fs = require("fs");
 const path = require("path");
 const { parse } = require("csv-parse");
 
+// Now after building planets.mongo.js that is where we need to save and persist our data,
+// let's connect our controller (API) with our mongodb to talk to collections but step by step
+// to avoid breaking our server
+
+const planets = require("./planets.mongo");
+
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
@@ -27,9 +33,19 @@ function loadPlanetsData() {
           columns: true,
         })
       )
-      .on("data", (data) => {
+      .on("data", async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          // habitablePlanets.push(data);
+          // Here whether to push the data to the habitablePlanets array, after requiring
+          // let's push data to our mongodb. collection--> document
+          /* TODO: REPLACE BELLOW CREATE WITH INSERT + UPDATE = UPSERT
+          await planets.create({
+            // create function here, creates a new document and pass the same data defined from the schema.
+            keplerName: data.keple_name,
+
+            // Now to avoid that this document will be created many times when we created many instances/clusters of server.js as the loadPlanetsData() is called many times
+            // Mongoose simply solved this problem using upsert func(insert + update): Insert + Update = upsert
+          }); */
         }
       })
       .on("error", (error) => {
--- server/src/models/planets.mongo.js
+++ server/src/models/planets.mongo.js
@@ -8,4 +8,7 @@ const planetSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("Planet", planetSchema);
+// The Model here compiles and allows us to use the schema. It's the model that allows
+// us to connect the schema to collections and we querry the model from the our server.
+// Mongoosse provides us these schema where we give data types to our data.
+module.exports = mongoose.model("Planet", planetSchema); // here we compiled a mongoose schema and assigned it to a collection in MongoDB.
